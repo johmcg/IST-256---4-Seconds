664ec73cd61461f7b9798ed50f7420c20a080115
johmcg <103613418+johmcg@users.noreply.github.com>
2023-03-27 10:14:13 -0400

    Delete js directory

--- js/toast.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("toastbtn")
-    .onclick = function () {
-    var toastElList = [].slice
-        .call(document.querySelectorAll('.toast'));
-    var toastList = toastElList.map(function (toastEl) {
-        return new bootstrap.Toast(toastEl)
-    })
-    toastList.forEach(toast => toast.show())
-}
